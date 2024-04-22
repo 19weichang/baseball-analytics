129cfcb92b3859338b54cfd74ca4b8ea9378c4c4
Jaime <jaime99619@gmail.com>
2024-04-22 17:49:38 +0800

    refac: 修改 player table

--- src/components/player/playerInfo.vue
+++ src/components/player/playerInfo.vue
@@ -15,9 +15,16 @@
           style="width: 100%"
           empty-text="暫無數據"
         >
-          <el-table-column fixed prop="number" label="No" width="50" />
-          <el-table-column prop="name" label="姓名" />
-          <el-table-column prop="age" label="年齡" />
+          <el-table-column fixed prop="year" label="球季" />
+          <el-table-column prop="pa" label="打數" />
+          <el-table-column prop="hit" label="安打" />
+          <el-table-column prop="double" label="二壘安打" />
+          <el-table-column prop="triple" label="三壘安打" />
+          <el-table-column prop="hr" label="全壘打" />
+          <el-table-column prop="rbi" label="打點" />
+          <el-table-column prop="run" label="得分" />
+          <el-table-column prop="bb" label="四壞" />
+          <el-table-column prop="so" label="三振" />
         </el-table>
       </div>
       <div v-else>暫無選手資料</div>
