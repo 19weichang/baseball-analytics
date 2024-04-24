f0e47620b0da11a4d13ec174d6a34a8910371841
Jaime <jaime99619@gmail.com>
2024-04-24 17:32:27 +0800

    feat: 添加 info tabs

--- src/components/player/playerInfo.vue
+++ src/components/player/playerInfo.vue
@@ -8,26 +8,32 @@
     @close="() => $emit('close')"
   >
     <div v-loading="infoLoading">
-      <div v-if="playerInfo">
-        <el-table
-          :data="playerInfo"
-          height="250"
-          style="width: 100%"
-          empty-text="暫無數據"
-        >
-          <el-table-column fixed prop="year" label="球季" />
-          <el-table-column prop="pa" label="打數" />
-          <el-table-column prop="hit" label="安打" />
-          <el-table-column prop="double" label="二壘安打" />
-          <el-table-column prop="triple" label="三壘安打" />
-          <el-table-column prop="hr" label="全壘打" />
-          <el-table-column prop="rbi" label="打點" />
-          <el-table-column prop="run" label="得分" />
-          <el-table-column prop="bb" label="四壞" />
-          <el-table-column prop="so" label="三振" />
-        </el-table>
-      </div>
-      <div v-else>暫無選手資料</div>
+      <el-tabs type="border-card">
+        <el-tab-pane label="生涯數據">
+          <div v-if="playerInfo">
+            <el-table
+              :data="playerInfo"
+              height="250"
+              style="width: 100%"
+              empty-text="暫無數據"
+            >
+              <el-table-column fixed prop="year" label="球季" />
+              <el-table-column prop="pa" label="打數" />
+              <el-table-column prop="hit" label="安打" />
+              <el-table-column prop="double" label="二壘安打" />
+              <el-table-column prop="triple" label="三壘安打" />
+              <el-table-column prop="hr" label="全壘打" />
+              <el-table-column prop="rbi" label="打點" />
+              <el-table-column prop="run" label="得分" />
+              <el-table-column prop="bb" label="四壞" />
+              <el-table-column prop="so" label="三振" />
+            </el-table>
+          </div>
+          <div v-else>暫無選手資料</div>
+        </el-tab-pane>
+        <el-tab-pane label="進階數據"></el-tab-pane>
+        <el-tab-pane label="對戰成績"></el-tab-pane>
+      </el-tabs>
     </div>
   </el-dialog>
 </template>
